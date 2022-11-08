8dad0720f79c902272957e89851400caab586586
Harsh Singh <51085015+hs9085298@users.noreply.github.com>
2022-11-08 09:59:24 +0530

    add: storybook integration and tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+  extend: {},
+  // get colors subsequently.
+  colors: {
+  "primary": "#000",
+  "primary-a10": "#111",
+  "primary-a24": "#333",
+  "primary-a75": "#444",
+  "primary-a99": "#666",
+  "secondary": "#888",
+  "secondary-a10": "#999",
+  "white": "#FFF",
+  "success": "#0761D1",
+  "success-a10": "#D3E5FF",
+  "danger": "#E60000",
+  "danger-a10": "#F7D4D6",
+  "warning": "#F5A623",
+  "warning-a10": "#FFEFCF",
+  "warning-a24": "#AB570A",
+  "black": "#1E1E24",
+  "black-a50": "#1E1E2470",
+  "grey": "#C4CECF",
+  }
+  },
+  variants: {
+  extend: {},
   },
   plugins: [],
-};
+  };
